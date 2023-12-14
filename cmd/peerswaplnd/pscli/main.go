8fe91db108323527d4b3b4a8e68e7ab1bbca0e7b
YusukeShimizu <bruwbird@mercari.com>
2023-12-14 13:58:48 +0900

    accept premium

--- cmd/peerswaplnd/pscli/main.go
+++ cmd/peerswaplnd/pscli/main.go
@@ -73,6 +73,11 @@ var (
 		Name:     "peer_pubkey",
 		Required: true,
 	}
+	maxAcceptablePremiumFlag = cli.Uint64Flag{
+		Name:     "max_acceptable_premium",
+		Usage:    "max acceptable premium for a swap",
+		Required: true,
+	}
 
 	swapOutCommand = cli.Command{
 		Name:  "swapout",
@@ -81,6 +86,7 @@ var (
 			satAmountFlag,
 			channelIdFlag,
 			assetFlag,
+			maxAcceptablePremiumFlag,
 		},
 		Action: swapOut,
 	}
@@ -92,6 +98,7 @@ var (
 			satAmountFlag,
 			channelIdFlag,
 			assetFlag,
+			maxAcceptablePremiumFlag,
 		},
 		Action: swapIn,
 	}
@@ -213,9 +220,10 @@ func swapIn(ctx *cli.Context) error {
 	defer cleanup()
 
 	res, err := client.SwapIn(context.Background(), &peerswaprpc.SwapInRequest{
-		ChannelId:  ctx.Uint64(channelIdFlag.Name),
-		SwapAmount: ctx.Uint64(satAmountFlag.Name),
-		Asset:      ctx.String(assetFlag.Name),
+		ChannelId:         ctx.Uint64(channelIdFlag.Name),
+		SwapAmount:        ctx.Uint64(satAmountFlag.Name),
+		Asset:             ctx.String(assetFlag.Name),
+		AcceptablePremium: ctx.Int64(maxAcceptablePremiumFlag.Name),
 	})
 	if err != nil {
 		return err
@@ -233,9 +241,10 @@ func swapOut(ctx *cli.Context) error {
 	defer cleanup()
 
 	res, err := client.SwapOut(context.Background(), &peerswaprpc.SwapOutRequest{
-		ChannelId:  ctx.Uint64(channelIdFlag.Name),
-		SwapAmount: ctx.Uint64(satAmountFlag.Name),
-		Asset:      ctx.String(assetFlag.Name),
+		ChannelId:         ctx.Uint64(channelIdFlag.Name),
+		SwapAmount:        ctx.Uint64(satAmountFlag.Name),
+		Asset:             ctx.String(assetFlag.Name),
+		AcceptablePremium: ctx.Int64(maxAcceptablePremiumFlag.Name),
 	})
 	if err != nil {
 		return err
